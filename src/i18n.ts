d9bf66ebf187bdd11fc5a9271798f4e24e26ca54
islandpirate7 <jordan.guillermo@gmail.com>
2025-03-13 02:25:35 -0400

    Remove legal directory and fix i18n.ts deprecation warning

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,11 +1,8 @@
 import { getRequestConfig } from 'next-intl/server';
  
-export default getRequestConfig(async ({locale, requestLocale}) => {
-  // Use requestLocale instead of locale parameter to fix the deprecation warning
-  const resolvedLocale = await requestLocale();
-  
+export default getRequestConfig(async ({ locale }) => {
   return {
-    locale: resolvedLocale,
-    messages: (await import(`./messages/${resolvedLocale}.json`)).default
+    locale,
+    messages: (await import(`./messages/${locale}.json`)).default
   };
 });
